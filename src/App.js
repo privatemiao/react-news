7a71ca47cdd68af9f11f0d403ee0dd4154defa7b
Mel Liu <privatemiao@gmail.com>
2017-11-28 08:29:09 +0800

    :lollipop:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import PCIndex from './js/components/pc_index';
+
+class App extends Component {
+    render() {
+        return (
+            <PCIndex></PCIndex>
+        );
+    }
+}
+
+export default App;
