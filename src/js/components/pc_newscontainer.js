b3e4cebf7d9bd07a328ae5b0132485d517983c6b
Mel Liu <privatemiao@gmail.com>
2017-12-03 01:38:41 +0800

    ~

--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Col, Icon, Menu, Carousel, Row, Tabs, Form, Input, message, Button, Checkbox, Modal} from 'antd';
 
-import carousel_1  from "../../image/carousel_1.jpg";
-import carousel_2  from "../../image/carousel_2.jpg";
-import carousel_3  from "../../image/carousel_3.jpg";
-import carousel_4  from "../../image/carousel_4.jpg";
+import carousel_1 from "../../image/carousel_1.jpg";
+import carousel_2 from "../../image/carousel_2.jpg";
+import carousel_3 from "../../image/carousel_3.jpg";
+import carousel_4 from "../../image/carousel_4.jpg";
+
+import PCNewsBlock from './pc_news_block';
 
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
@@ -32,9 +34,16 @@ export default class PCNewsContainer extends React.Component {
                                     <div><img src={carousel_3}/></div>
                                     <div><img src={carousel_4}/></div>
                                 </Carousel>
-
                             </div>
                         </div>
+                        <Tabs className="tabs_news">
+                            <TabPane tab={"新闻"} key={"1"}>
+                                <PCNewsBlock count={15} type={"top"} width={"100%"} bordered={"false"}/>
+                            </TabPane>
+                            <TabPane tab={"国际"} key={"2"}>
+                                <PCNewsBlock count={15} type={"guoji"} width={"100%"} bordered={"false"}/>
+                            </TabPane>
+                        </Tabs>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
