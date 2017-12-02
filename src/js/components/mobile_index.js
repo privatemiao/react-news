41246d7f9f651ed4ff42eaea7db1d5831abeb892
Mel Liu <privatemiao@gmail.com>
2017-12-03 02:38:43 +0800

    ~

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
-import {Row, Col, Menu, Icon, Tabs, message, Form, Input, Button, CheckBox, Modal} from 'antd';
+import {CheckBox, Tabs} from 'antd';
+import MobileList from "./mobile_list";
 
 const TabPane = Tabs.TabPane;
 export default class MobileIndex extends React.Component {
@@ -11,16 +12,16 @@ export default class MobileIndex extends React.Component {
                 <MobileHeader/>
                 <Tabs>
                     <TabPane tab={"头条"} key={"1"}>
-
+                        <MobileList count={20} type={"top"}/>
                     </TabPane>
                     <TabPane tab={"社会"} key={"2"}>
-
+                        <MobileList count={20} type={"shehui"}/>
                     </TabPane>
                     <TabPane tab={"国内"} key={"3"}>
-
+                        <MobileList count={20} type={"guonei"}/>
                     </TabPane>
                     <TabPane tab={"国际"} key={"4"}>
-
+                        <MobileList count={20} type={"guoji"}/>
                     </TabPane>
                 </Tabs>
                 <MobileFooter/>
